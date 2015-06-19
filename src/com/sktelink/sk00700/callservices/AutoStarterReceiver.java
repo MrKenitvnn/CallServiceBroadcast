0de25ba3439d9be40c24a8d33f8bb875e7b71393
ken.trams <mry.itvnn@gmail.com>
2015-06-19 16:46:18 +0700

    a

--- src/com/sktelink/sk00700/callservices/AutoStarterReceiver.java
+++ src/com/sktelink/sk00700/callservices/AutoStarterReceiver.java
@@ -7,9 +7,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
 
